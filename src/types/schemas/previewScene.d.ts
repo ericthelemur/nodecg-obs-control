6bbc4477cbea75d53e509a407526ff4036275446
Owen Connors <owenconnors1@gmail.com>
2023-12-26 01:25:23 +0000

    Update schema types to match websocket v5

--- src/types/schemas/previewScene.d.ts
+++ src/types/schemas/previewScene.d.ts
@@ -11,20 +11,50 @@ export type PreviewScene = null | {
 };
 
 export interface ObsSource {
-	alignment?: number;
-	cx: number;
-	cy: number;
-	id: number;
-	locked: boolean;
-	muted?: boolean;
-	name: string;
-	render: boolean;
-	source_cx: number;
-	source_cy: number;
-	type: string;
-	volume: number;
-	x: number;
-	y: number;
-	parentGroupName?: string;
-	groupChildren?: unknown[];
+	inputKind: null | string;
+	isGroup: boolean | null;
+	sceneItemBlendMode:
+		| 'OBS_BLEND_NORMAL'
+		| 'OBS_BLEND_ADDITIVE'
+		| 'OBS_BLEND_SUBTRACT'
+		| 'OBS_BLEND_SCREEN'
+		| 'OBS_BLEND_MULTIPLY'
+		| 'OBS_BLEND_LIGHTEN'
+		| 'OBS_BLEND_DARKEN';
+	sceneItemEnabled: boolean;
+	sceneItemId: number;
+	sceneItemIndex: number;
+	sceneItemLocked: boolean;
+	sceneItemTransform: ObsTransform;
+	sourceName: string;
+	sourceType: string;
+	[k: string]: unknown;
+}
+export interface ObsTransform {
+	positionX: number;
+	positionY: number;
+	width: number;
+	height: number;
+	sourceHeight: number;
+	sourceWidth: number;
+	rotation: number;
+	scaleX: number;
+	scaleY: number;
+	alignment: number;
+	boundsType:
+		| 'OBS_BOUNDS_NONE'
+		| 'OBS_BOUNDS_STRETCH'
+		| 'OBS_BOUNDS_SCALE_INNER'
+		| 'OBS_BOUNDS_SCALE_OUTER'
+		| 'OBS_BOUNDS_SCALE_TO_WIDTH'
+		| 'OBS_BOUNDS_SCALE_TO_HEIGHT'
+		| 'OBS_BOUNDS_MAX_ONLY';
+	boundsAlignment: number;
+	boundsWidth: number;
+	boundsHeight: number;
+	cropLeft: number;
+	cropRight: number;
+	cropTop: number;
+	cropBottom: number;
+	[k: string]: unknown;
 }
